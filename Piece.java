1cde32647ba0ec5b9ac2ce23d080f05b0728aa67
Sanmitr564 <generic39542@gmail.com>
2021-12-16 12:28:15 -0800

    inital commit

---
+++ Piece.java
@@ -0,0 +1,11 @@
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+class Piece{
+    private int piece;
+    private Vector2 position;
+    public Piece(int piece, Vector2 position){
+        this.piece = piece;
+        this.position = position;
+    }
+    
+}
